cb447fc051a34ca74862b15dc37a0d880fe29564
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-02-01 19:30:05 +0100

    overclock: remove clock domain ratios unthrottling before overclocking as it creates bottlenecks on clock domains, review the code for better testing and stability

--- experimental-overclock/main.cpp
+++ experimental-overclock/main.cpp
@@ -82,10 +82,10 @@ int _setOverclock() {
   int intr;
   suspendCpuIntr(intr);
   
-  // throttle all clock domains before overclocking PLL
-  hw(0xbc200000) = 32 << 16 | 511;
-  hw(0xBC200004) = 32 << 16 | 511;
-  hw(0xBC200008) = 32 << 16 | 511;
+  // set clock domains to ratio 1:1
+  hw(0xbc200000) = 511 << 16 | 511;
+  hw(0xBC200004) = 511 << 16 | 511;
+  hw(0xBC200008) = 511 << 16 | 511;
   sync();
     
   // set bit bit 7 to apply index, wait until hardware clears it
@@ -104,12 +104,6 @@ int _setOverclock() {
     _num++;
   }
 
-  // unthrottle all clock domains after overclocking PLL
-  hw(0xbc200000) = 511 << 16 | 511;
-  hw(0xBC200004) = 511 << 16 | 511;
-  hw(0xBC200008) = 511 << 16 | 511;
-  sync();
-
   // wait for clock stability, signal propagation and pipeline drain
   {
     u32 i = 0xfffff;
@@ -123,6 +117,8 @@ int _setOverclock() {
   return 0;
 }
 
+unsigned int numtest = 0;
+unsigned int dentest = 0;
 void _cancelOverclock() {
   
   const int TARGET_FREQUENCY = 333;
@@ -131,65 +127,75 @@ void _cancelOverclock() {
   const u32 den = 19;
   const float base = 37;
 
-  const u32 num = (u32)(((float)(TARGET_FREQUENCY * den)) / (base * ratio));
-  u32 _num = (u32)(((float)(THEORETICAL_FREQUENCY * den)) / (base * ratio));
+  const unsigned int num = (u32)(((float)(TARGET_FREQUENCY * den)) / (base * ratio));
+  // u32 _num = (u32)(((float)(THEORETICAL_FREQUENCY * den)) / (base * ratio));
   
   int intr;
   suspendCpuIntr(intr);
   
-  u32 index = 2;
-  hw(0xbc200000) = 32 << 16 | 511;
-  hw(0xBC200004) = 32 << 16 | 511;
-  hw(0xBC200008) = 32 << 16 | 511;
-  sync();
-  
-  hw(0xbc100068) = 0x80 | index;
-  do {
-    delayPipeline();
-  } while (hw(0xbc100068) != index);
-  
-  while (_num > num) {
-    const u32 lsb = _num << 8 | den;
-    const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
-    hw(0xbc1000fc) = multiplier;
-    delayPipeline();
-    _num--;
-  }
-
-  index = 5;
-  hw(0xbc100068) = 0x80 | index;
-  do {
-    delayPipeline();
-  } while (hw(0xbc100068) != index);
-
   hw(0xbc200000) = 511 << 16 | 511;
   hw(0xBC200004) = 511 << 16 | 511;
   hw(0xBC200008) = 511 << 16 | 511;
   sync();
 
-  u32 i = 0xfffff;
-  while (--i) {
-    delayPipeline();
+  const u32 pllMul = hw(0xbc1000fc);
+  const u32 msb = pllMul & 0xffff;
+  const u32 _den = msb & 0xff;
+  u32 _num = msb >> 8;
+  
+  if (_den && ((_num / _den) > 10)) {
+    // u32 index = 2;
+    /*
+    hw(0xbc100068) = 0x80 | index;
+    do {
+      delayPipeline();
+    } while (hw(0xbc100068) != index);
+    */
+    
+    while (_num > num) {
+      _num--;
+      const u32 lsb = _num << 8 | den;
+      const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
+      hw(0xbc1000fc) = multiplier;
+      delayPipeline();
+    }
+
+    const int index = 5;
+    hw(0xbc100068) = 0x80 | index;
+    do {
+      delayPipeline();
+    } while (hw(0xbc100068) != index);
+
+    u32 i = 0xfffff;
+    while (--i) {
+      delayPipeline();
+    }
+    
+    resumeCpuIntr(intr);
+    
+    scePowerSetClockFrequency(TARGET_FREQUENCY, TARGET_FREQUENCY, TARGET_FREQUENCY/2);
   }
-  
-  resumeCpuIntr(intr);
-  
-  scePowerSetClockFrequency(TARGET_FREQUENCY, TARGET_FREQUENCY, TARGET_FREQUENCY/2);
 }
 
 int main() {
   pspDebugScreenInit();
+  pspDebugScreenSetXY(1, 1);
   
   if (pspSdkLoadStartModule("./kcall.prx", PSP_MEMORY_PARTITION_KERNEL) < 0) {
-    pspDebugScreenSetXY(1, 1);
     pspDebugScreenPrintf("Can't load kcall prx");
     sceKernelExitGame();
     return 0;
   }
 
+  pspDebugScreenPrintf("Overclock Sample");
   cancelOverclock();
   setOverclock();
+  pspDebugScreenClear();
+  
   // scePowerSetClockFrequency(333, 333, 166);
+
+  u32 switchOverclock = 0;
+  unsigned int lastFreq = 444;
 
   u64 prev, now, fps = 0, maxFps = 0, counter = 0;
   const u64 res = sceRtcGetTickResolution();
@@ -200,15 +206,35 @@ int main() {
     sceRtcGetCurrentTick(&prev);
     
     sceCtrlPeekBufferPositive(&ctl, 1);
-    
-    maxFps = fps > maxFps ? fps : maxFps;
+    maxFps = (fps > maxFps) ? fps : maxFps;
     
     pspDebugScreenSetXY(1, 1);
     pspDebugScreenPrintf("MaxFPS: %llu, FPS: %llu    ", maxFps, fps);
     
     pspDebugScreenSetXY(1, 2);
     pspDebugScreenPrintf("Counter: %llu    ", counter++);
-        
+    
+    if (!switchOverclock && (ctl.Buttons & PSP_CTRL_TRIANGLE)) {
+      const int freq = lastFreq == 333 ? 444 : 333;
+      if (freq == 444) {
+        setOverclock();
+        lastFreq = 444;
+      }
+      else {
+        cancelOverclock();
+        lastFreq = 333;
+      }
+      switchOverclock = 1;
+      maxFps = 0;
+    }
+    else if(!(ctl.Buttons & PSP_CTRL_TRIANGLE)) {
+      switchOverclock = 0;
+    }
+    
+    pspDebugScreenSetXY(1, 3);
+    pspDebugScreenPrintf("Switch to %u MHZ", lastFreq);
+    
+    //
     sceRtcGetCurrentTick(&now);
     fps = res / (now - prev);
     
