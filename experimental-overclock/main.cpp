32bbe957be675de1cdff52ef69a8e13f273703a3
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-02-03 19:27:35 +0100

    check overclock status in 0xbc1000fc via custom flag, use EI bit instead of mt/mfic to suspend/resume interupt, remove dead code, improve comments

--- experimental-overclock/main.cpp
+++ experimental-overclock/main.cpp
@@ -21,41 +21,38 @@ PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
 
 // modify this value to compare results
 #define DEFAULT_FREQUENCY     333
-#define THEORETICAL_FREQUENCY (444 /*+ 37/4*/) /*for 2k+*/
-//#define THEORETICAL_FREQUENCY (370 /*+ 37/4*/) /*for 1k*/
+#define THEORETICAL_FREQUENCY 444 /*for 2k+*/
+//#define THEORETICAL_FREQUENCY 370 /*for 1k*/
 #define PLL_MUL_MSB           0x0124
 #define PLL_BASE_FREQ         37
-#define PLL_DEN               19
+#define PLL_DEN               19 /*21*/
+#define PLL_RATIO_INDEX       5
+#define PLL_RATIO             1.0f
 
 // Note: Only tested on Slim, should be called from SC side
 // 444 ok - approaching the stability limit
-// base * (num / den) * ratio, with base = 37 and ratio = 1
+// float a = (float)(THEORETICAL_FREQUENCY / BASE)
+// PLL_DEN = (int)((255.0f / a) * PLL_RATIO)
+// PLL_NUM = (int)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO_VALUE))
+// PLL_FREQ = PLL_BASE_FREQ * (PLL_NUM / PLL_DEN) * PLL_RATIO, with PLL_BASE_FREQ = 37 and PLL_RATIO = 1.0f
+
 int _setOverclock() {
   
-  // note: needs to be 333 to be able to reach 444mhz
-  const int INITIAL_FREQUENCY = DEFAULT_FREQUENCY;
-  scePowerSetClockFrequency(INITIAL_FREQUENCY, INITIAL_FREQUENCY, INITIAL_FREQUENCY/2);
+  u32 _num = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
+  const u32 num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
 
-  // set index, to pll ratio 1
-  const u32 index = 5; // hw(0xbc100068) & 0xf;
-  float ratio = 1.0f;
-  switch (index) {
-    // incomplete
-    case 5: ratio = 1.0f; break;
-  }
-  
-  const u32 num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * ratio));
-  u32 _num = (u32)(((float)(INITIAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * ratio));
+  // note: needs to be 333 to be able to reach 444mhz
+  scePowerSetClockFrequency(DEFAULT_FREQUENCY, DEFAULT_FREQUENCY, DEFAULT_FREQUENCY/2);
 
   int intr;
   suspendCpuIntr(intr);
   resetDomains();
   
   // set bit bit 7 to apply index, wait until hardware clears it
-  hw(0xbc100068) = 0x80 | index;
+  hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
   do {
     delayPipeline();
-  } while (hw(0xbc100068) != index);
+  } while (hw(0xbc100068) != PLL_RATIO_INDEX);
   
   // loop until the numerator reaches the target value,
   // and so, progressively increasing clock frequencies
@@ -64,8 +61,11 @@ int _setOverclock() {
     const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
     hw(0xbc1000fc) = multiplier;
     sync();
-    _num++;
+    _num += 1;
   }
+  
+  hw(0xbc1000fc) |= (1 << 16);
+  sync();
   
   settle();
   resetDomains();
@@ -76,13 +76,15 @@ int _setOverclock() {
 
 void _cancelOverclock() {
   
-  const int TARGET_FREQUENCY = DEFAULT_FREQUENCY;
-  const float ratio = 1.0f;
-  const unsigned int num = (u32)(((float)(TARGET_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * ratio));
-
+  
+  u32 _num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
+  const unsigned int num = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
+  // const unsigned int limit = (unsigned int)(9.0f * (1.0f / PLL_RATIO));
+  
   int intr;
   suspendCpuIntr(intr);
   
+  /*
   const u32 pllIdx = hw(0xbc100068);
   const u32 pllMul = hw(0xbc1000fc);
   sync();
@@ -91,35 +93,54 @@ void _cancelOverclock() {
   const u32 _den = msb & 0xff;
   u32 _num = msb >> 8;
   
-  const int overclocked = (int)(_den && ((_num / _den) > 9));
+  const int overclocked = (int)(_den && ((_num / _den) > 9)); //limit
   
-  if (overclocked && pllIdx == 5) {
-    
+  if (overclocked && pllIdx == PLL_RATIO_INDEX) {
+  */
+  const u32 pllMul = hw(0xbc1000fc);
+  sync();
+
+  const int overclocked = pllMul & (1 << 16);
+  
+  if (overclocked) {
+  
     resetDomains();
     
     while (_num > num) {
-      _num--;
+      _num -= 1;
       const u32 lsb = _num << 8 | PLL_DEN;
       const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
       hw(0xbc1000fc) = multiplier;
       sync();
     }
     
-    const int index = 5;
-    hw(0xbc100068) = 0x80 | index;
+    hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
     do {
       delayPipeline();
-    } while (hw(0xbc100068) != index);
+    } while (hw(0xbc100068) != PLL_RATIO_INDEX);
     
     settle();
     resetDomains();
   }
   
   resumeCpuIntr(intr);
-
+  
   //if (overclocked) {
   //  scePowerSetClockFrequency(TARGET_FREQUENCY, TARGET_FREQUENCY, TARGET_FREQUENCY/2);
   //}
+}
+
+#define dumpPLLRegs()                                                          \
+  sceKernelIcacheInvalidateAll();                                              \
+  kcall((int (*)(void))(0x80000000 | (unsigned int)_dumpPLLRegs));
+
+unsigned int pllCtl = 0;
+unsigned int pllMul = 0;
+int _dumpPLLRegs() {
+  pllCtl = hw(0xbc100068);
+  pllMul = hw(0xbc1000fc);
+  sync();
+  return 0;
 }
 
 int main() {
@@ -133,9 +154,9 @@ int main() {
   }
 
   pspDebugScreenPrintf("Overclock Sample");
+  dumpPLLRegs();
   cancelOverclock();
   // setOverclock();
-  // scePowerSetClockFrequency(333, 333, 166);
   pspDebugScreenClear();
 
   u32 switchOverclock = 0;
@@ -177,6 +198,10 @@ int main() {
     
     pspDebugScreenSetXY(1, 3);
     pspDebugScreenPrintf("Switch to %u MHz", lastFreq);
+    
+    pspDebugScreenSetXY(0, 5);
+    pspDebugScreenPrintf("pllCtl: %x    \n", pllCtl);
+    pspDebugScreenPrintf("pllMul: %x    \n", pllMul);
     
     //
     sceRtcGetCurrentTick(&now);
--- experimental-overclock/main.h
+++ experimental-overclock/main.h
@@ -13,34 +13,77 @@
     ".set reorder                      \n" \
   )
 
+#define suspendCpuIntr(var)    \
+  asm volatile(                \
+    ".set push             \n" \
+    ".set noreorder        \n" \
+    ".set noat             \n" \
+    "mfc0  %0, $12         \n" \
+    "sync                  \n" \
+    "li    $k0, 0xFFFFFFFE \n" \
+    "and   $k0, %0, $k0    \n" \
+    "mtc0  $k0, $12        \n" \
+    "sync                  \n" \
+    "nop                   \n" \
+    "nop                   \n" \
+    "nop                   \n" \
+    ".set pop              \n" \
+    : "=r"(var)                \
+    :                          \
+    : "$k0", "memory"          \
+  )
+
+#define resumeCpuIntr(var) \
+  asm volatile(            \
+    ".set push      \n"    \
+    ".set noreorder \n"    \
+    ".set noat      \n"    \
+    "mtc0  %0, $12  \n"    \
+    "sync           \n"    \
+    "nop            \n"    \
+    "nop            \n"    \
+    "nop            \n"    \
+    ".set pop       \n"    \
+    :                      \
+    : "r"(var)             \
+    : "memory"             \
+  )
+
+/*
 #define suspendCpuIntr(var) \
   asm volatile(             \
+    ".set push      \n"     \
     ".set noreorder \n"     \
+    ".set noat      \n"     \
     "mfic  %0, $0   \n"     \
+    "sync           \n"     \
     "mtic  $0, $0   \n"     \
     "sync           \n"     \
     "nop            \n"     \
     "nop            \n"     \
     "nop            \n"     \
-    ".set reorder   \n"     \
+    ".set pop       \n"     \
     : "=r"(var)             \
     :                       \
-    : "$8", "memory"        \
+    : "memory"              \
   )
 
 #define resumeCpuIntr(var)  \
   asm volatile(             \
+    ".set push      \n"     \
     ".set noreorder \n"     \
+    ".set noat      \n"     \
     "mtic  %0, $0   \n"     \
     "sync           \n"     \
     "nop            \n"     \
     "nop            \n"     \
     "nop            \n"     \
-    ".set reorder   \n"     \
+    ".set pop       \n"     \
     :                       \
     : "r"(var)              \
     : "memory"              \
   )
+*/
 
 // Set clock domains to ratio 1:1
 #define resetDomains()               \
