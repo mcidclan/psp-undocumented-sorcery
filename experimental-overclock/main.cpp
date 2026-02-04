b3c019deca6f0e169bf1da625e1a9267da6cd95c
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-02-04 14:13:11 +0100

    overclock: rewrite critical sections in assembly, reorganise the code, make sure adding a delay between overclock and cancel overclock processes

--- experimental-overclock/main.cpp
+++ experimental-overclock/main.cpp
@@ -12,20 +12,24 @@ PSP_HEAP_SIZE_KB(-1024);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
 
 #define setOverclock()                                                         \
-  sceKernelIcacheInvalidateAll();                                              \
   kcall((int (*)(void))(0x80000000 | (unsigned int)_setOverclock));
 
 #define cancelOverclock()                                                      \
-  sceKernelIcacheInvalidateAll();                                              \
   kcall((int (*)(void))(0x80000000 | (unsigned int)_cancelOverclock));
+
+#define unlockMemory()                                                         \
+  kcall((int (*)(void))(0x80000000 | (unsigned int)_unlockMemory));
+
+#define resetDomainRatios()                                                    \
+  kcall((int (*)(void))(0x80000000 | (unsigned int)_resetDomains));
 
 // modify this value to compare results
 #define DEFAULT_FREQUENCY     333
-#define THEORETICAL_FREQUENCY 444 /*for 2k+*/
+#define THEORETICAL_FREQUENCY (444 /*+ 37/2*/) /*for 2k+*/
 //#define THEORETICAL_FREQUENCY 370 /*for 1k*/
 #define PLL_MUL_MSB           0x0124
 #define PLL_BASE_FREQ         37
-#define PLL_DEN               19 /*21*/
+#define PLL_DEN               20
 #define PLL_RATIO_INDEX       5
 #define PLL_RATIO             1.0f
 
@@ -37,37 +41,43 @@ PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
 // PLL_FREQ = PLL_BASE_FREQ * (PLL_NUM / PLL_DEN) * PLL_RATIO, with PLL_BASE_FREQ = 37 and PLL_RATIO = 1.0f
 
 int _setOverclock() {
-  
+
   u32 _num = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
   const u32 num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
 
   // note: needs to be 333 to be able to reach 444mhz
-  scePowerSetClockFrequency(DEFAULT_FREQUENCY, DEFAULT_FREQUENCY, DEFAULT_FREQUENCY/2);
-
+  //scePowerSetClockFrequency(DEFAULT_FREQUENCY, DEFAULT_FREQUENCY, DEFAULT_FREQUENCY/2);
+  
   int intr;
   suspendCpuIntr(intr);
-  resetDomains();
-  
-  // set bit bit 7 to apply index, wait until hardware clears it
-  hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
+
+  // set bit bit 7 to apply index
+  // then wait until hardware clears bit 7
+  processPLL();
+  /*
+  hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX; sync();
   do {
     delayPipeline();
   } while (hw(0xbc100068) != PLL_RATIO_INDEX);
-  
+  */
+
   // loop until the numerator reaches the target value,
   // and so, progressively increasing clock frequencies
+  increasePLL();
+  /*
   while (_num <= num) {
     const u32 lsb = _num << 8 | PLL_DEN;
     const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
     hw(0xbc1000fc) = multiplier;
-    delayPipeline(); /*sync();*/
-    _num += 1;
+    sync();
+    _num++;
   }
+  */
   
+  // add custom overclock state flag
   hw(0xbc1000fc) |= (1 << 16);
-  
   settle();
-  resetDomains();
+  
   resumeCpuIntr(intr);
 
   return 0;
@@ -77,50 +87,51 @@ void _cancelOverclock() {
   
   u32 _num = (u32)(((float)(THEORETICAL_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
   const unsigned int num = (u32)(((float)(DEFAULT_FREQUENCY * PLL_DEN)) / (PLL_BASE_FREQ * PLL_RATIO));
-  
+
   int intr;
   suspendCpuIntr(intr);
   
-  const u32 pllMul = hw(0xbc1000fc);
-  sync();
-
+  const u32 pllMul = hw(0xbc1000fc); sync();
   const int overclocked = pllMul & (1 << 16);
   
   if (overclocked) {
-  
-    resetDomains();
     
-    while (_num > num) {
-      _num -= 1;
+    // loop until the numerator reaches the target value,
+    // and so, progressively increasing clock frequencies
+    decreasePLL();
+    /*
+    while (_num >= num) {
       const u32 lsb = _num << 8 | PLL_DEN;
       const u32 multiplier = (PLL_MUL_MSB << 16) | lsb;
       hw(0xbc1000fc) = multiplier;
-      delayPipeline(); /*sync();*/
+      sync();
+      _num--;
     }
+    */
     
-    hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX;
+    // set bit bit 7 to apply index
+    // then wait until hardware clears bit 7
+    processPLL();
+    /*
+    hw(0xbc100068) = 0x80 | PLL_RATIO_INDEX; sync();
     do {
       delayPipeline();
     } while (hw(0xbc100068) != PLL_RATIO_INDEX);
+    */
     
     settle();
-    resetDomains();
   }
-  
   resumeCpuIntr(intr);
 }
 
-#define dumpPLLRegs()                                                          \
-  sceKernelIcacheInvalidateAll();                                              \
-  kcall((int (*)(void))(0x80000000 | (unsigned int)_dumpPLLRegs));
-
-unsigned int pllCtl = 0;
-unsigned int pllMul = 0;
-int _dumpPLLRegs() {
-  pllCtl = hw(0xbc100068);
-  pllMul = hw(0xbc1000fc);
-  sync();
-  return 0;
+void intOverclock() {
+  sceKernelIcacheInvalidateAll();
+  unlockMemory();
+  
+  resetDomainRatios();
+  // note: needs to be 333 to be able to reach 444mhz
+  scePowerSetClockFrequency(DEFAULT_FREQUENCY, DEFAULT_FREQUENCY, DEFAULT_FREQUENCY/2);
+  cancelOverclock();
 }
 
 int main() {
@@ -134,13 +145,11 @@ int main() {
   }
 
   pspDebugScreenPrintf("Overclock Sample");
-  dumpPLLRegs();
-  cancelOverclock();
-  // setOverclock();
+  intOverclock();
   pspDebugScreenClear();
 
   u32 switchOverclock = 0;
-  unsigned int lastFreq = DEFAULT_FREQUENCY; //THEORETICAL_FREQUENCY;
+  unsigned int lastFreq = DEFAULT_FREQUENCY;
 
   u64 prev, now, fps = 0, maxFps = 0, counter = 0;
   const u64 res = sceRtcGetTickResolution();
@@ -171,6 +180,7 @@ int main() {
       }
       switchOverclock = 1;
       maxFps = 0;
+      sceKernelDelayThread(500000);
     }
     else if(!(ctl.Buttons & PSP_CTRL_TRIANGLE)) {
       switchOverclock = 0;
@@ -179,11 +189,6 @@ int main() {
     pspDebugScreenSetXY(1, 3);
     pspDebugScreenPrintf("Switch to %u MHz", lastFreq);
     
-    pspDebugScreenSetXY(0, 5);
-    pspDebugScreenPrintf("pllCtl: %x    \n", pllCtl);
-    pspDebugScreenPrintf("pllMul: %x    \n", pllMul);
-    
-    //
     sceRtcGetCurrentTick(&now);
     fps = res / (now - prev);
     
--- experimental-overclock/main.h
+++ experimental-overclock/main.h
@@ -8,21 +8,20 @@
 
 #define delayPipeline()                    \
   asm volatile(                            \
-    ".set noreorder                    \n" \
     "nop; nop; nop; nop; nop; nop; nop \n" \
-    ".set reorder                      \n" \
   )
 
 #define suspendCpuIntr(var)    \
   asm volatile(                \
     ".set push             \n" \
     ".set noreorder        \n" \
+    ".set volatile         \n" \
     ".set noat             \n" \
     "mfc0  %0, $12         \n" \
     "sync                  \n" \
-    "li    $k0, 0xFFFFFFFE \n" \
-    "and   $k0, %0, $k0    \n" \
-    "mtc0  $k0, $12        \n" \
+    "li    $t0, 0x0FFFFFFE \n" \
+    "and   $t0, %0, $t0    \n" \
+    "mtc0  $t0, $12        \n" \
     "sync                  \n" \
     "nop                   \n" \
     "nop                   \n" \
@@ -30,13 +29,14 @@
     ".set pop              \n" \
     : "=r"(var)                \
     :                          \
-    : "$k0", "memory"          \
+    : "$t0", "memory"          \
   )
 
 #define resumeCpuIntr(var) \
   asm volatile(            \
     ".set push      \n"    \
     ".set noreorder \n"    \
+    ".set volatile  \n"    \
     ".set noat      \n"    \
     "mtc0  %0, $12  \n"    \
     "sync           \n"    \
@@ -49,53 +49,163 @@
     : "memory"             \
   )
 
-/*
-#define suspendCpuIntr(var) \
-  asm volatile(             \
-    ".set push      \n"     \
-    ".set noreorder \n"     \
-    ".set noat      \n"     \
-    "mfic  %0, $0   \n"     \
-    "sync           \n"     \
-    "mtic  $0, $0   \n"     \
-    "sync           \n"     \
-    "nop            \n"     \
-    "nop            \n"     \
-    "nop            \n"     \
-    ".set pop       \n"     \
-    : "=r"(var)             \
-    :                       \
-    : "memory"              \
+#define resetDomains()          \
+  asm volatile(                 \
+    ".set push              \n" \
+    ".set noreorder         \n" \
+    ".set nomacro           \n" \
+    ".set volatile          \n" \
+    ".set noat              \n" \
+                                \
+    "  lui  $t0, 0xBC20     \n" \
+    "  lui  $t1, 0xFF       \n" \
+    "  ori  $t1, $t1, 0x1FF \n" \
+    "  sw   $t1, 0x00($t0)  \n" \
+    "  sw   $t1, 0x04($t0)  \n" \
+    "  sw   $t1, 0x08($t0)  \n" \
+    "  sync                 \n" \
+                                \
+    ".set pop               \n" \
+    :                           \
+    :                           \
+    : "$t0", "$t1", "memory"    \
   )
 
-#define resumeCpuIntr(var)  \
-  asm volatile(             \
-    ".set push      \n"     \
-    ".set noreorder \n"     \
-    ".set noat      \n"     \
-    "mtic  %0, $0   \n"     \
-    "sync           \n"     \
-    "nop            \n"     \
-    "nop            \n"     \
-    "nop            \n"     \
-    ".set pop       \n"     \
-    :                       \
-    : "r"(var)              \
-    : "memory"              \
+#define settle()                \
+  asm volatile(                 \
+    ".set push              \n" \
+    ".set noreorder         \n" \
+    ".set nomacro           \n" \
+    ".set volatile          \n" \
+    ".set noat              \n" \
+                                \
+    "sync                   \n" \
+    "lui  $t0, 0x20         \n" \
+    "ori  $t0, $t0, 0xFFFF  \n" \
+                                \
+    "1:                     \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  addiu $t0, $t0, -1   \n" \
+    "  bnez  $t0, 1b        \n" \
+    "  nop                  \n" \
+                                \
+    ".set pop               \n" \
+    :                           \
+    :                           \
+    : "$t0", "memory"           \
   )
-*/
 
-// Set clock domains to ratio 1:1
-#define resetDomains()               \
-  hw(0xbc200000) = 511 << 16 | 511;  \
-  /*hw(0xBC200004) = 511 << 16 | 511;*/  \
-  /*hw(0xBC200008) = 511 << 16 | 511;*/  \
+static inline int _resetDomains() {
+  resetDomains();
+  return 0;
+}
+
+static inline void _unlockMemory() {
+  const u32 start = 0xbc000000;
+  const u32 end   = 0xbc00002c;
+  for (u32 reg = start; reg <= end; reg += 4) {
+    hw(reg) = -1;
+  }
   sync();
+}
 
-// Wait for clock stability, signal propagation and pipeline drain
-#define settle()            \
-  sync();                   \
-  u32 i = 0x1fffff;         \
-  while (--i) {             \
-    delayPipeline();        \
-  }                         
+#define increasePLL()           \
+  asm volatile(                 \
+    ".set push              \n" \
+    ".set noreorder         \n" \
+    ".set nomacro           \n" \
+    ".set volatile          \n" \
+    ".set noat              \n" \
+                                \
+    "1:                     \n" \
+    "  sll  $t0, %0, 8      \n" \
+    "  ori  $t0, $t0, %1    \n" \
+    "  lui  $t1, %2         \n" \
+    "  or   $t1, $t1, $t0   \n" \
+                                \
+    "  lui  $t0, 0xBC10     \n" \
+    "  sw   $t1, 0xFC($t0)  \n" \
+    "  sync                 \n" \
+                                \
+    "  addiu %0, %0, 1      \n" \
+    "  sltu  $t0, %3, %0    \n" \
+    "  beqz  $t0, 1b        \n" \
+    "  nop                  \n" \
+                                \
+    ".set pop               \n" \
+    : "+r"(_num)                                \
+    : "i"(PLL_DEN), "i"(PLL_MUL_MSB), "r"(num)  \
+    : "$t0", "$t1", "memory"                    \
+  )
+  
+#define decreasePLL()           \
+  asm volatile(                 \
+    ".set push              \n" \
+    ".set noreorder         \n" \
+    ".set nomacro           \n" \
+    ".set volatile          \n" \
+    ".set noat              \n" \
+                                \
+    "1:                     \n" \
+    "  sll  $t0, %0, 8      \n" \
+    "  ori  $t0, $t0, %1    \n" \
+    "  lui  $t1, %2         \n" \
+    "  or   $t1, $t1, $t0   \n" \
+                                \
+    "  lui  $t0, 0xBC10     \n" \
+    "  sw   $t1, 0xFC($t0)  \n" \
+    "  sync                 \n" \
+                                \
+    "  addiu %0, %0, -1     \n" \
+    "  sltu  $t0, %0, %3    \n" \
+    "  beqz  $t0, 1b        \n" \
+    "  nop                  \n" \
+                                \
+    ".set pop               \n" \
+    : "+r"(_num)                               \
+    : "i"(PLL_DEN), "i"(PLL_MUL_MSB), "r"(num) \
+    : "$t0", "$t1", "memory"                   \
+  )
+
+#define processPLL()            \
+  asm volatile(                 \
+    ".set push              \n" \
+    ".set noreorder         \n" \
+    ".set nomacro           \n" \
+    ".set volatile          \n" \
+    ".set noat              \n" \
+                                \
+    "  lui  $t0, 0xBC10     \n" \
+    "  ori  $t1, $zero, %0  \n" \
+    "  ori  $t1, $t1, 0x80  \n" \
+    "  sw   $t1, 0x68($t0)  \n" \
+    "  sync                 \n" \
+                                \
+    "1:                     \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+    "  nop                  \n" \
+                                \
+    "  lw    $t1, 0x68($t0) \n" \
+    "  ori   $t1, $t1, 0    \n" \
+    "  xori  $t1, $t1, %0   \n" \
+    "  sync                 \n" \
+                                \
+    "  bnez  $t1, 1b        \n" \
+    "  nop                  \n" \
+                                \
+    ".set pop               \n" \
+    :                           \
+    : "i"(PLL_RATIO_INDEX)      \
+    : "$t0", "$t1", "memory"    \
+  )
